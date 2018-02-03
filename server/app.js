07cdeb06901ef750c7c5236eb1ab2ca76a324625
Santiago Iraola <iraola.santi@gmail.com>
2018-02-03 14:49:23 -0800

    docker minor changes

--- server/app.js
+++ server/app.js
@@ -2,12 +2,13 @@ require('newrelic');
 const express = require('express');
 let app = express();
 let router = require('./routes');
+const pid = process.pid;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get('/test', (req, res) => {
-  res.json({test: true});
+  res.json({instance: pid});
 });
 
 app.use('/', router);
